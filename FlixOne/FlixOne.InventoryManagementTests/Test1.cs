6142c5699af6d9514b73dce8de4f7454540f2f90
@Ky777 <kondr777@live.ru>
2026-04-23 17:05:16 +0300

    add: create project FlixOne.InventoryManagementTests(close: #1 )

---
+++ FlixOne/FlixOne.InventoryManagementTests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace FlixOne.InventoryManagementTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
