09824f68c1c6443a04f70a71ada2863900df8305
@Ky777 <kondr777@live.ru>
2026-05-13 19:40:35 +0300

    add: interface iservicecollection (close: #19)

---
+++ FlixOne/FlixOne.InventoryManagementClient/ConsoleUserInterface.cs
@@ -0,0 +1,29 @@
+﻿using FlixOne.InventoryManagement.UserInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlixOne.InventoryManagementClient
+{
+    internal class ConsoleUserInterface:IUserInterface
+    {
+        public string ReadValue(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+
+        public void WriteMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+        }
+
+        public void WriteWarning(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(message);
+        }
+    }
+}
--- FlixOne/FlixOne.InventoryManagementClient/Program.cs
+++ FlixOne/FlixOne.InventoryManagementClient/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace FlixOne.InventoryManagementClient
+﻿using FlixOne.InventoryManagement.Command;
+using FlixOne.InventoryManagement.UserInterface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlixOne.InventoryManagementClient
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IServiceCollection services = new ServiceCollection();
+            services.AddTransient<IUserInterface, ConsoleUserInterface>();
+            services.AddTransient<IInventoryCommandFactory, InventoryCommandFactory>();
         }
     }
 }
